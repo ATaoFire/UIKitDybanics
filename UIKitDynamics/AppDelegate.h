1346ae23aa6db464b576d2e7c0501e0a1479affb
ztmengyuxuan <583299127@qq.com>
2016-09-18 09:18:48 +0800

    Initial Commit

---
+++ UIKitDynamics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIKitDynamics
+//
+//  Created by 张涛 on 16/9/18.
+//  Copyright © 2016年 张涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
